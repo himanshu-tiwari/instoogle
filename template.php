536e37d7a9e4a34a303dcf6427c2e24cca08fb13
himanshu-tiwari <htiwarih@gmail.com>
2015-12-06 19:25:29 +0530

    Update template.php

--- template.php
+++ template.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8"/>
-    <title>geogram</title>
+    <title>instoogle</title>
   </head>
   <body>
   <form action="/REST/index.php" method="get">
